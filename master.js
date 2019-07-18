c9d8770b0925c5cd081c0f3fd748e65d51d63fb9
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-18 18:06:51 +0200

    Menù Drop down funzionanti

---
+++ master.js
@@ -0,0 +1,15 @@
+//Gestione dei menu Drop-Box nell HEADER
+var linkElement = document.getElementById('nav-bar-right');
+var dropClik = $('a');
+var dropMenu = $('.drop');
+
+console.log(dropClik);
+
+//Azione del clic sul link per far comparire il menù drop box
+dropClik.click(function(){
+
+  var thisMenu = $(this).next();
+
+  thisMenu.toggleClass("active");
+
+});
